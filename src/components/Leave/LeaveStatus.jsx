a70e7f860316a852bf8577c41030a8bfe68fa125
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-10-12 19:03:20 +0530

    address edit, leave status

--- src/components/Leave/LeaveStatus.jsx
+++ src/components/Leave/LeaveStatus.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Card } from '@mui/material'
 import SideBar from '../../pages/Sidebar/Sidebar';
 
 const LeaveStatus = () => {
